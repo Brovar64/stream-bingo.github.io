cbcc02653d03763e9416adf1d35d720e7d5c6a55
Brovar64 <brovar64@gmail.com>
2025-03-14 14:19:09 +0100

    Remove sensitive configuration values

--- js/config.js
+++ js/config.js
@@ -1,16 +1,16 @@
-// Global configuration object
+// Placeholder configuration to be replaced during deployment
 var CONFIG = {
   FIREBASE: {
-    apiKey: 'AIzaSyC6HO0LPJwI4tlWYlYSxw2IEGUu6Fu-fOU',
-    authDomain: 'stream-bingo-ecb40.firebaseapp.com',
-    projectId: 'stream-bingo-ecb40',
-    storageBucket: 'stream-bingo-ecb40.appspot.com',
-    messagingSenderId: '814141306111',
-    appId: '1:814141306111:web:319ac02de11210186408ca',
-    measurementId: 'G-1DYM2MTE10'
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+    measurementId: ''
   },
   TWITCH: {
-    CLIENT_ID: 'k53e9s8oc2leprhcgyoa010e38bm6s',
-    REDIRECT_URI: 'https://brovar64.github.io/stream-bingo.github.io/index.html'
+    CLIENT_ID: '',
+    REDIRECT_URI: ''
   }
 };
